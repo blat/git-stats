de68294dc6aeb9d5bd4301235e0ff5ab3992881b
blat <blatiere@gmail.com>
2010-10-23 17:52:16 +0200

    fixes and code cleaning

--- git-stats.rb
+++ git-stats.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'date'
+require 'digest/md5'
 require 'erb'
 require 'git'
 require 'sinatra'
@@ -20,23 +21,36 @@ get '/:repository/:since' do
     git = Git.open('data/' + @repository)
 
     @by_author = {}
-    @by_date = Array.new(@since, 0)
-    @by_hour = Array.new(24, 0)
-    @by_day_of_week = Array.new(7, 0)
+
+    @by_date = {}
+    (0..@since).each do |n|
+        @by_date[(Date.today - @since + n).to_s] = 0
+    end
+
+    @by_hour = {}
+    (0..23).each do |n|
+        @by_hour[n.to_s + "h"] = 0
+    end
+
+    @by_day_of_week = {}
+    (0..6).each do |n|
+        @by_day_of_week[Date::DAYNAMES[n]] = 0
+    end
 
     commits = git.log.since(@since.to_s + ' days ago')
     commits.each do |commit|
 
         author = commit.author.name
+        date = DateTime.strptime(commit.date.strftime("%Y-%m-%d %H:%M:%S"), "%Y-%m-%d %H:%M:%S");
+
         if not @by_author.key?(author) then
             @by_author[author] = 0
         end
         @by_author[author] += 1
 
-        date = Date.strptime(commit.date.strftime("%Y-%m-%d"), "%Y-%m-%d");
-        @by_date[Date.today - date] += 1
-        @by_hour[commit.date.strftime("%H").to_i] += 1
-        @by_day_of_week[date.wday] += 1
+        @by_date[date.strftime("%Y-%m-%d")] += 1
+        @by_hour[date.hour.to_s + "h"] += 1
+        @by_day_of_week[Date::DAYNAMES[date.wday]] += 1
 
     end
 
